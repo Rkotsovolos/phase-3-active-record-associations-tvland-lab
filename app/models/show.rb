c70579e09dae1c15c277515894adea2207c48f78
Ryan Kotsovolos <rkotsovolos@gmail.com>
2021-12-04 14:08:56 -0800

    commit tree 10e726c223bccd2e1ed11fa6d7ed8dc366f963fa

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
-  
+    
+    has_many :characters
+    belongs_to :network
+    has_many :actors, through: :characters 
+
+    def actors_list
+        self.actors.map do |actor|
+            first = actor.first_name 
+            last = actor.last_name
+            "#{first} #{last}"
+        end
+    end
+
 end
